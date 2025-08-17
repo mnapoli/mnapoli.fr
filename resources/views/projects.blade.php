8ccfbe9c617a0ee8a72f53b29ed6aea5f6eaa2a8
Matthieu Napoli <matthieu@mnapoli.fr>
2025-08-17 20:55:07 +0200

    Add favicon.show project

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -351,6 +351,30 @@
             </p>
         </li>
 
+        <li class="group relative flex flex-col items-start">
+            <h2 class="text-base w-full font-semibold text-zinc-800 dark:text-zinc-100">
+                <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
+                <a href="https://favicon.show">
+                    <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+                    <span class="relative z-10">favicon.show</span>
+                </a>
+            </h2>
+            <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400 line-clamp-2">
+                Universal favicon URL. Running on Cloudflare Workers.
+            </p>
+            <div class="relative z-10 mt-2 text-xs font-semibold text-zinc-400 dark:text-zinc-400 flex items-center gap-4">
+                <div>
+                    $0/month
+                </div>
+            </div>
+            <img class="relative z-10 mt-4 w-full object-cover aspect-video rounded-2xl shadow-lg transition-transform transform group-hover:scale-110"
+                 src="/images/projects/favicon-show.png">
+            <p class="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                <svg viewBox="0 0 24 24" aria-hidden="true" class="h-6 w-6 flex-none"><path d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z" fill="currentColor"></path></svg>
+                <span class="ml-2">favicon.show</span>
+            </p>
+        </li>
+
     </ul>
 
 @endsection
